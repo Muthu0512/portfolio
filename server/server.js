0d7f2d926b93d47e1f8ccef6657a0db17505292f
Muthu0512 <masmuthu99@gmail.com>
2026-06-03 15:52:37 +0530

    sever.js modified

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT
 if (process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"/client/dist")))
 
-    app.get("(*)",(req,res)=>{
+    app.get("*splat",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
     })
 }
